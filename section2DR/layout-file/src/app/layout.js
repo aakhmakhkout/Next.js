f820d52820b8d1fabfabe1bafece2d248acbfb3f
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-06-28 11:19:21 +0530

    learning how to make reusuable layouts (similar to nested routing with outlet)

--- section2DR/layout-file/src/app/layout.js
+++ section2DR/layout-file/src/app/layout.js
@@ -4,9 +4,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-black flex flex-col justify-between items-center h-screen">
-        <header className="font-bold text-3xl pt-10">Header</header>
+        <header className="font-bold text-3xl mt-10 bg-[purple] p-[5px_30px] rounded-lg">Header</header>
         {children}
-        <footer className="font-bold text-3xl pb-10">Footer</footer>
+        <footer className="font-bold text-3xl mb-10 bg-[purple] p-[5px_30px] rounded-lg">Footer</footer>
       </body>
     </html>
   );
