b500d51511287fe43381dee93a7a37960c7fcb42
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-06-30 22:09:40 +0530

    learned private routes, how to create folders without making them routes, like component and other folders

---
+++ section2Extra/private-routes/src/app/_privateComponent/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function privateComponent() {
+  return (
+    <div>privateComponent</div>
+  )
+}
---
+++ section2Extra/private-routes/src/app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div className='flex w-full h-screen justify-center items-center font-bold text-5xl'> 
+        <h1>Home page</h1>
+    </div>
+  )
+}
