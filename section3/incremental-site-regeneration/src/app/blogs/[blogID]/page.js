d8659b779dc45e6c0953f34f7f88532fe969a346
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-07 13:03:37 +0530

    learned incremental site regeneration, how to use revalidate, how isr works in foreground and background, how it serves the regenerated page etc

---
+++ section3/incremental-site-regeneration/src/app/blogs/[blogID]/page.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Link from "next/link"
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  // return [
+  //   {blogID: "1"},
+  //   {blogID: "2"},
+  //   {blogID: "3"},
+  //   {blogID: "4"},
+  //   {blogID: "5"},
+  // ]
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+  const data = await response.json()
+  return data.map(({id})=> ({blogID: String(id)}))
+}
+
+export const revalidate = 3
+
+export default async function page({params}) {
+    const {blogID} =  await params
+    console.log("Blog ID = ", blogID)
+
+    const dateAndTime = new Date().toLocaleString()
+    console.log(dateAndTime)
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link ">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link active">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Welcome to Our blog number {blogID}</h1>
+        <p>This is the blog number {blogID}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus error temporibus laudantium ea explicabo eius autem, dolores eaque doloribus sint.</p>
+        <h1>Date and time : {dateAndTime}</h1>
+      </div>
+    </>
+  );
+};
+
