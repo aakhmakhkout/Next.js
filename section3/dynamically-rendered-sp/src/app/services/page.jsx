35952a2f4a121f0f05b1dd0331d7ff687fe64b3a
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-09 12:32:39 +0530

    Learned how to make static pages dynamic using multiple dynamic API's like dynamic = force-dynamic, cookies(), searchParams. also learned force-static, error etc

--- section3/dynamically-rendered-sp/src/app/services/page.jsx
+++ section3/dynamically-rendered-sp/src/app/services/page.jsx
@@ -1,9 +1,22 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-const Services = () => {
+//? 1 = dynamic = "force-dynamic", 2 = searchParams (with await), 3 = cookies()
+
+export const dynamic = "force-dynamic";
+// export const dynamic = "force-static";
+// export const dynamic = "error"; // error during build, make page static but don't ignore dynamic api's like cookies, searchParams etc
+
+const Services = async ({ searchParams }) => {
+  console.log("Services Page");
+  // const search = await searchParams;
+  // console.log(search);
+
+  const myCookies = await cookies();
+  console.log(myCookies);
   return (
     <>
-       <nav>
+      <nav>
         <ul className="navbar">
           <li>
             <Link href="/" className="nav-link">
