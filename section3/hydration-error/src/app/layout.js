e8a635da897db6cfab6f0fc0306fa4ff2992c534
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-15 17:06:05 +0000

    Learned hydration and hydration error, what is hydration, why does it happen, how it happenes, etc, and also real DOM, virtual DOM etc

---
+++ section3/hydration-error/src/app/layout.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import "./globals.css";
+
+export const metadata = {
+  title: "next-app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
