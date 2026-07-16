31927a0fb9a39df4c636aa3db74eab47aaaf0cf3
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-16 22:46:43 +0530

    updated comments.jsx in hydration error

--- section3/hydration-error/components/Comments.jsx
+++ section3/hydration-error/components/Comments.jsx
@@ -1,16 +1,16 @@
 "use client";
 
 export default function Comments() {
-  if (typeof window === "undefined") {
-    return (
-      <div className="flex justify-center items-center bg-[orange] w-80 h-30 gap-3 capitalize rounded-xl">
-        total comments : 50k
-      </div>
-    );
-  }
+  // if (typeof window === "undefined") {
+  //   return (
+  //     <div className="flex justify-center items-center bg-[orange] w-80 h-30 gap-3 capitalize rounded-xl">
+  //       total comments : 50k
+  //     </div>
+  //   );
+  // }
   return (
     <div className="flex justify-center items-center bg-black/80 w-80 h-30 gap-3 capitalize rounded-xl">
-      total comments : 1k
+      total comments : 1k {Date.now()}
     </div>
   );
 }
