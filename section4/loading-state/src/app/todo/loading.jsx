ba002c2bc32883107cbe69758f2377c20a9d6ced
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-19 22:58:16 +0530

    Learned how to handle loading state via loading.jsx file inside the same folder which we want to show laoding

---
+++ section4/loading-state/src/app/todo/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col w-full h-screen justify-center items-center">
+      <p className="animate-pulse text-xl font-bold">Loading...</p>
+    </div>
+  );
+}
